fe862b53d7c4c52bcca8086a648a5d7e18af052c
Wojciech Dudek <wdudek82@gmail.com>
2020-08-22 21:56:03 +0200

    Extend Swagger UI security definition description

--- ParkyAPIApp/ConfigureSwaggerOptions.cs
+++ ParkyAPIApp/ConfigureSwaggerOptions.cs
@@ -33,7 +33,10 @@ namespace ParkyAPIApp
 
             options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
             {
-                Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then",
+                Description =
+                    "JWT Authorization header using the Bearer scheme. \r\n\r\n " +
+                    " Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\n " +
+                    "Example: \"Bearer 12345abcdef\"",
                 Name = "Authorization",
                 In = ParameterLocation.Header,
                 Type = SecuritySchemeType.ApiKey,
